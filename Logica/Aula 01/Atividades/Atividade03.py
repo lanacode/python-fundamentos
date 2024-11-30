3166c34fb419aee63179370e507cb5e299fc59be
Ilana Nascimento - ARTDR <artedasredes@gmail.com>
2024-11-30 13:04:59 -0300

    Create Atividade03.py

---
+++ Logica/Aula 01/Atividades/Atividade03.py
@@ -0,0 +1,14 @@
+nota1 = input('Digite a nota da primeira avaliação: ')
+nota2 = input('Digite a nota da primeira avaliação: ')
+nota3 = input('Digite a nota da primeira avaliação: ')
+nota4 = input('Digite a nota da primeira avaliação: ')
+
+nota1 = float(nota1)
+nota2 = float(nota2)
+nota3 = float(nota3)
+nota4 = float(nota4)
+
+soma = nota1 + nota2 + nota3 + nota4
+resultado = soma/4
+
+print(f'Sua media é {resultado}')
