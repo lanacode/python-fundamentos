48722e0d4d8e5fa87546cbb7e53d21f5ef874798
Ilana Nascimento - ARTDR <artedasredes@gmail.com>
2024-11-30 13:04:22 -0300

    Create Atividade01.py

---
+++ Logica/Aula 01/Atividades/Atividade01.py
@@ -0,0 +1,9 @@
+num1 = input('Digite o primeiro numero:')
+num2 = input('Digite o segundo numero:')
+
+num1 = int(num1)
+num2 = int(num2)
+
+resultado = num1 + num2
+
+print('o resultado é:',resultado)
