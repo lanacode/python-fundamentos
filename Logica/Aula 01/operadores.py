5c062a7138a4be69e5fb5b49136493cc8dd9aebc
Ilana Nascimento - ARTDR <artedasredes@gmail.com>
2024-11-30 13:03:05 -0300

    Create operadores.py

---
+++ Logica/Aula 01/operadores.py
@@ -0,0 +1,9 @@
+num1 = input('Digite o primeiro numero:')
+num2 = input('Digite o segundo numero:')
+
+num1 = int(num1)
+num2 = int(num2)
+
+soma = num2 + num1
+
+print(f'O resultado é igual a:{soma}')
