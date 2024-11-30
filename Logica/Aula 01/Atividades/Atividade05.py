d8f400a17a30a8ea2d5ef7404abe528186f03d9d
Ilana Nascimento - ARTDR <artedasredes@gmail.com>
2024-11-30 13:05:43 -0300

    Create Atividade05.py

---
+++ Logica/Aula 01/Atividades/Atividade05.py
@@ -0,0 +1,6 @@
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+idade = int(idade)
+cidadeNatal = input('Digite sua cidade natal: ')
+
+print(f'Olá {nome}, você tem {idade} anos e nasceu na cidade de {cidadeNatal}')
