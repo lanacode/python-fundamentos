81e285168f7e1a702fd208687679ca2950abcdff
Ilana Nascimento - ARTDR <artedasredes@gmail.com>
2024-11-30 13:03:29 -0300

    Create tiposDeDados.py

---
+++ Logica/Aula 01/tiposDeDados.py
@@ -0,0 +1,20 @@
+#tipos de dados
+
+#numeros inteiros
+idade = 19
+
+#float (numeros que tem parte decimal)
+altura: 1.62
+preco = 10.00
+
+#string (texto)
+nome = 'Ilana'
+sobrenome = "Nascimento"
+citacao = 'João falou: "não gostei!"'
+
+#boolean (lógicos)
+passouDeAno = False
+senhaCorreta = True
+
+#variaveis podem começar com letra maiuscula e _, 
+# além da minuscula mas nunca com numero
